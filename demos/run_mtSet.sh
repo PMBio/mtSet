a32071dec8dc78f800e65e43c2bcefa9be5db056
paolo.casale <casale@ebi.ac.uk>
2014-12-01 16:38:50 +0000

    option renaming

--- demos/run_mtSet.sh
+++ demos/run_mtSet.sh
@@ -5,7 +5,7 @@ PFILE=./out/pheno
 WFILE=./out/windows
 NFILE=./out/null
 WSIZE=10000
-OUTDIR=./out/results
+RESDIR=./out/results
 OUTFILE=./out/final
 
 # Preprocessing and generation
@@ -15,13 +15,13 @@ OUTFILE=./out/final
 
 # Analysis
 # test
-./../mtSet/bin/mtSet_analyze --bfile $BFILE --cfile $CFILE --pfile $PFILE --nfile $NFILE --wfile $WFILE --minSnps 4 --outdir $OUTDIR --start_wnd 0 --end_wnd 5
-./../mtSet/bin/mtSet_analyze --bfile $BFILE --cfile $CFILE --pfile $PFILE --nfile $NFILE --wfile $WFILE --minSnps 4 --outdir $OUTDIR --start_wnd 5 --end_wnd 10
+./../mtSet/bin/mtSet_analyze --bfile $BFILE --cfile $CFILE --pfile $PFILE --nfile $NFILE --wfile $WFILE --minSnps 4 --resdir $RESDIR --start_wnd 0 --end_wnd 5
+./../mtSet/bin/mtSet_analyze --bfile $BFILE --cfile $CFILE --pfile $PFILE --nfile $NFILE --wfile $WFILE --minSnps 4 --resdir $RESDIR --start_wnd 5 --end_wnd 10
 #permutations
 for i in `seq 0 1`;
 do
-./../mtSet/bin/mtSet_analyze --bfile $BFILE --cfile $CFILE --pfile $PFILE --nfile $NFILE --wfile $WFILE --minSnps 4 --outdir $OUTDIR --start_wnd 0 --end_wnd 5 --perm $i
-./../mtSet/bin/mtSet_analyze --bfile $BFILE --cfile $CFILE --pfile $PFILE --nfile $NFILE --wfile $WFILE --minSnps 4 --outdir $OUTDIR --start_wnd 5 --end_wnd 10 --perm $i
+./../mtSet/bin/mtSet_analyze --bfile $BFILE --cfile $CFILE --pfile $PFILE --nfile $NFILE --wfile $WFILE --minSnps 4 --resdir $RESDIR --start_wnd 0 --end_wnd 5 --perm $i
+./../mtSet/bin/mtSet_analyze --bfile $BFILE --cfile $CFILE --pfile $PFILE --nfile $NFILE --wfile $WFILE --minSnps 4 --resdir $RESDIR --start_wnd 5 --end_wnd 10 --perm $i
 done
 
 #postprocess
