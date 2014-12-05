4e09ef3d105837f5bb63042fecd4ac899be6f6e0
paolo.casale <casale@ebi.ac.uk>
2014-12-05 10:40:24 +0000

    get data deleted

--- demos/get_data.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash 
-wget http://www.ebi.ac.uk/~casale/1000G_chr22.zip
-mkdir ./../data
-unzip 1000G_chr22.zip -d ./../data
-rm 1000G_chr22.zip
--- demos/run_all.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash 
-./get_data.sh
-./run_mtSet.sh
